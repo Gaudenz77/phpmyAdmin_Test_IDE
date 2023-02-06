62fa5631e5aa1dda8092bc4e8f8c06950d7905c8
Gaudenz@Krakatoom <42372680+Gaudenz77@users.noreply.github.com>
2023-02-06 14:03:25 +0100

    cnahged connection variables case sens problem

--- includes/db.php
+++ includes/db.php
@@ -1,11 +1,11 @@
 <?php
 // connect to mysql, using php-PDO-Object
 $dbHost = getenv('DB_HOST');
-$dbName = getenv('DB_NAME');
+$dbname = getenv('DB_NAME');
 $dbUser = getenv('DB_USER');
 $dbPassword = getenv('DB_PASSWORD');
 
-$dbConnection = new PDO("mysql:host=$dbHost;dbName=$dbName;charset=utf8",$dbUser,$dbPassword);
+$dbConnection = new PDO("mysql:host=$dbHost;dbname=$dbname;charset=utf8",$dbUser,$dbPassword);
 // set ERROR-MODE for webDevs ("->" calls function in class-object (new PDO (see above), "::" is a static variable, like a constant-variable)
 $dbConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
 
